bdf6932e2a0ecfbadd9743830a1c1a5309f2d634
cjrcooper <Coops@Christophers-MacBook-Pro-2.local>
2016-04-17 14:55:38 +1000

    Added in more models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416055517) do
+ActiveRecord::Schema.define(version: 20160417033805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
+    t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.string   "string"
     t.integer  "phone_number"
     t.money    "balance",      scale: 2
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.money    "price",       scale: 2
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "orders", force: :cascade do |t|
