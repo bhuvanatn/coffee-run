a428e7c7178d3c055c306053d3c7ca0c49bae0ac
Bill Cruickshank <jestingrabbit@gmail.com>
2016-04-18 10:20:51 +1000

    backbone models for users - probably need to modify urls

---
+++ app/assets/javascripts/models/store.js
@@ -0,0 +1,8 @@
+var app = app || {};
+
+app.Store = {
+  defaults: {
+
+  }
+  urlRoot: "/stores",
+}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417074946) do
+ActiveRecord::Schema.define(version: 20160417234248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 20160417074946) do
     t.money    "price",       scale: 2
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "quantity"
+    t.integer  "order_id"
+    t.string   "notes"
+    t.money    "unit_price", scale: 2
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "orders", force: :cascade do |t|
