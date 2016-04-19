79d4cb8ca1832ddd8beb1defa6538911a2638074
David Haynes <dhaynesxx@gmail.com>
2016-04-19 20:15:58 +1000

    adjusted orderlist view removing doubleups

--- app/assets/javascripts/views/OrderListView.js
+++ app/assets/javascripts/views/OrderListView.js
@@ -8,59 +8,37 @@ app.OrderListView = Backbone.View.extend({
 
       var userType = app.currentUser.attributes.type;
 
-      if (userType === "Store"){
-        orderListViewTemplate = $('#StoreOrderListViewTemplate').html();
+      orderListViewTemplate = $('#' + userType + 'OrderListViewTemplate').html();
 
-        var orderViewHTML = _.template(orderListViewTemplate);
-          for (var i = 0; i < app.orders.length; i++){
+      var orderViewHTML = _.template(orderListViewTemplate);
+        for (var i = 0; i < app.orders.length; i++){
             var order = app.orders.models[i].attributes;
 
             for (var j = 0; j < app.stores.length; j++){
-              var storeId = app.stores.models[j].attributes.id;
-              if (order.store_id === storeId){
-                order.storeName = app.stores.models[j].attributes.name;
-              }
+                var storeId = app.stores.models[j].attributes.id;
+                if (order.store_id === storeId){
+
+                    order.storeName = app.stores.models[j].attributes.name;
+                }
             }
             for (var k = 0; k < app.stores.length; k++){
-              var customerId = app.customers.attributes[k].id;
-              if (order.customer_id === customerId){
-                order.customerName = app.customers.attributes[k].name;
-              }
+                var customerId = app.customers.attributes[k].id;
+                if (order.customer_id === customerId){
+                    order.customerName = app.customers.attributes[k].name;
+                }
             }
             var orderElement = orderViewHTML(order);
-            if (app.current_user.attributes.id === order.store_id){
-            this.$el.append(orderElement);
-          }
-        }
-
-
-
-      } else if (userType === "Runner") {
-        orderListViewTemplate = $('#RunnerOrderListViewTemplate').html();
-
-        var orderViewHTML = _.template(orderListViewTemplate);
-          for (var i = 0; i < app.orders.length; i++){
-            var order = app.orders.models[i].attributes;
-
-            for (var j = 0; j < app.stores.length; j++){
-              var storeId = app.stores.models[j].attributes.id;
-              if (order.store_id === storeId){
-                order.storeName = app.stores.models[j].attributes.name
-              }
-            }
-            for (var k = 0; k < app.stores.length; k++){
-              var customerId = app.customers.attributes[k].id;
-              if (order.customer_id === customerId){
-                order.customerName = app.customers.attributes[k].name
-              }
-            }
-              if (app.orders.models[i].attributes.runner_id === null){
-                var orderElement = orderViewHTML(order);
+            if (app.orders.models[i].attributes.runner_id === null){
                 this.$el.append(orderElement);
-              }
             }
-        } else {
-        orderListViewTemplate = "";
+            else if (app.current_user.attributes.id === order.store_id){
+                this.$el.append(orderElement);
+            }
+            else {
+                orderListViewTemplate = "";
+            }
+        }
       }
-    }
+
+
   });
