a2146a528ad3ca8930155fc3c9a1a3c25a647bee
Bhuvana <bhuvanatn@gmail.com>
2016-04-20 11:06:40 +1000

    OrderView

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,60 +1,69 @@
 var app = app || {};
 
 app.AppRouter = Backbone.Router.extend({
-  routes: {
-    '': 'introduction',
-    'stores' : 'showStoreList',
-    'menu/:id' : 'showMenu',
-    'orderlist' : 'showOrderList',
-    'order/:id' : 'showOrder'
-  },
+    routes: {
+        '': 'introduction',
+        'stores': 'showStoreList',
+        'menu/:id': 'showMenu',
+        'orderlist': 'showOrderList',
+        'order/:id': 'showOrder'
+    },
 
-  introduction: function() {
-    var appView = new app.AppView();
-    appView.render();
-  },
-  
-  showStoreList: function () {
-    app.stores = new app.Stores();
-    app.stores.fetch().done( function () {
-      var storeListView = new app.StoreListView();
-      storeListView.render();
-    });
-  },
+    introduction: function() {
+        var appView = new app.AppView();
+        appView.render();
+    },
 
-  showMenu: function (id) {
-    app.store = new app.Store({id: id});
-    app.store.fetch().done( function () {
-      var menuView = new app.MenuView({model: app.store});
-      menuView.render();
-    }).error( function () {
-      console.log("wtf");
-    });
-  },
-
-  showOrderList: function () {
-    app.orders = new app.Orders();
-    app.stores = new app.Stores();
-    app.customers = new app.Customer();
-
-    app.orders.fetch().done( function () {
-    app.stores.fetch().done( function () {
-    app.customers.fetch().done( function() {
-      var orderListView = new app.OrderListView();
-      orderListView.render();
+    showStoreList: function() {
+        app.stores = new app.Stores();
+        app.stores.fetch().done(function() {
+            var storeListView = new app.StoreListView();
+            storeListView.render();
         });
-      });
-    });
-  },
+    },
 
-  showOrder: function (id) {
-    app.order = new app.Order({id: id});
-    app.order.fetch().done( function () {
-      var orderView = new app.OrderView({model: app.order});
-      orderView.render();
-    }).error( function () {
-      console.log('wtf');
-    });
-  }
+    showMenu: function(id) {
+        app.store = new app.Store({
+            id: id
+        });
+        app.store.fetch().done(function() {
+            var menuView = new app.MenuView({
+                model: app.store
+            });
+            menuView.render();
+        }).error(function() {
+            console.log("wtf");
+        });
+    },
 
+    showOrderList: function() {
+        app.orders = new app.Orders();
+        app.stores = new app.Stores();
+        app.customers = new app.Customer();
+
+        app.orders.fetch().done(function() {
+            app.stores.fetch().done(function() {
+                app.customers.fetch().done(function() {
+                    var orderListView = new app.OrderListView();
+                    orderListView.render();
+                });
+            });
+        });
+    },
+
+    showOrder: function(id) {
+        app.order = new app.Order({ id: id });
+        app.stores = new app.Stores();
+        app.customers = new app.Customer();
+        app.order.fetch().done(function() {
+            app.stores.fetch().done(function() {
+                app.customers.fetch().done(function() {
+                    var orderView = new app.OrderView({
+                        model: app.order
+                    });
+                    orderView.render();
+                });
+            });
+        });
+    }
 });
--- app/assets/javascripts/views/OrderView.js
+++ app/assets/javascripts/views/OrderView.js
@@ -2,11 +2,20 @@ var app = app || {};
 
 app.OrderView = Backbone.View.extend({
     el: '#main',
-
+    // Customer
+    //
+    // 1. Store list (#stores)
+    // 2. Store Menu (menu/:id)
+    // 3. Order (
+    //
+    // Store: The Store that the order was made at
+    // Customer Name: Who made the order
+    // Items: All items they requested (quantity)
+    // Price: Total price of the items
+    // )
     render: function() {
-        var orders = new app.Orders();
-        orders.fetch().done(function() {
-            console.log(lineItems);
-        });
+        $('#main').html('');
+        orderViewTemplate = $('#OrderViewTemplate').html();
+        var orderViewHTML = _.template(orderViewTemplate);
     }
 });
