6d002f5e130bb9405ffffad6e18e55e2d6ed4129
David Haynes <dhaynesxx@gmail.com>
2016-04-21 12:12:52 +1000

    seed data fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,14 @@ s4 = Store.create name: 'Bunker', email: 'bunker@ga.co', password: 'chicken', pa
 
 
 Order.destroy_all
-o1 = Order.create total_price: 5.00 status: 'pending'
-o2 = Order.create total_price: 5.00 status: 'pending'
-o3 = Order.create total_price: 5.00 status: 'pending'
-o4 = Order.create total_price: 5.00 status: 'confirmed'
-o5 = Order.create total_price: 5.00 status: 'confirmed'
-o6 = Order.create total_price: 5.00 status: 'confirmed'
-o7 = Order.create total_price: 5.00 status: 'pickedUp'
-o8 = Order.create total_price: 5.00 status: 'delivered'
+o1 = Order.create total_price: 5.00, status: 'pending'
+o2 = Order.create total_price: 5.00, status: 'pending'
+o3 = Order.create total_price: 5.00, status: 'pending'
+o4 = Order.create total_price: 5.00, status: 'confirmed'
+o5 = Order.create total_price: 5.00, status: 'confirmed'
+o6 = Order.create total_price: 5.00, status: 'confirmed'
+o7 = Order.create total_price: 5.00, status: 'pickedUp'
+o8 = Order.create total_price: 5.00, status: 'delivered'
 
 Item.destroy_all
 i1 = Item.create name: 'Flat White', price: 3.0, store_id: s1.id
@@ -38,30 +38,24 @@ i9 = Item.create name: 'Long Black', price: 2.7
 LineItem.destroy_all
 l1 = LineItem.create quantity: 1
 l2 = LineItem.create quantity: 1
+l3 = LineItem.create quantity: 1
 l4 = LineItem.create quantity: 1
 l5 = LineItem.create quantity: 1
 l6 = LineItem.create quantity: 1
 l7 = LineItem.create quantity: 1
 l8 = LineItem.create quantity: 1
 l9 = LineItem.create quantity: 1
+l10 = LineItem.create quantity: 1
+l11 = LineItem.create quantity: 1
 
 
 s1.items << i1 << i4 << i7
 s2.items << i2 << i5 << i8
 s3.items << i3 << i6 << i9
 
-l1.items << i1
-l2.items << i4
-l3.items << i7
-l4.items << i1
-l5.items << i4
-l6.items << i7
-l7.items << i1
-l8.items << i4
-l9.items << i7
-l10.items << i1
-l11.items << i4
-
+i1.line_items << l1 << l4 << l7 << l10
+i4.line_items << l2 << l5 << l8 << l11
+i7.line_items << l3 << l6 << l9
 
 o1.line_items << l1 << l2
 o2.line_items << l3
