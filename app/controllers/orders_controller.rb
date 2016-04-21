61bcca7bbe05452fbdacbb413b05851fc204fc34
cjrcooper <Coops@Christophers-MacBook-Pro-2.local>
2016-04-21 11:54:43 +1000

    Updated order lists

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -70,6 +70,6 @@ class OrdersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
 
     def order_params
-      params.require(:order).permit(:customer_id, :runner_id, :store_id, :total_price)
+      params.require(:order).permit(:customer_id, :runner_id, :store_id, :total_price, :status)
     end
 end
